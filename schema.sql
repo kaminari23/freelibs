544d7ba1cfb01781acf5bd754bebf558471d862b
kaminari23 <kurokaminari23@gmail.com>
2020-06-19 01:42:55 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE booklist (
   annotation VARCHAR NOT NULL,
   author VARCHAR(50) NOT NULL,
   picture VARCHAR(500) NOT NULL,
-  capacity INT(20) NOT NULL,
+  capacity INT NOT NULL,
   pubdate DATE NOT NULL,
   PRIMARY KEY (id)
 );
